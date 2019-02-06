e053fdb0163d1cb4e80dfbe5d0f07ab3e403ad4c
SchulungsTeilnehmer <Torsten.Lubomirski@sage.com>
2019-02-06 11:30:15 +0100

    bla

--- DummyProjekt/MainWindow.xaml.cs
+++ DummyProjekt/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -18,11 +19,50 @@ namespace DummyProjekt
     /// <summary>
     /// Interaktionslogik für MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
+        System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (dispatcherTimer.IsEnabled)
+            {
+                dispatcherTimer.Stop();
+            }
+            else
+            {
+                dispatcherTimer.Start();
+            }
+        }
+        // https://stackoverflow.com/questions/6084398/pick-a-random-brush
+        private Brush PickBrush()
+        {
+            Brush result = Brushes.Transparent;
+
+            Random rnd = new Random();
+
+            Type brushesType = typeof(Brushes);
+
+            PropertyInfo[] properties = brushesType.GetProperties();
+
+            int random = rnd.Next(properties.Length);
+            result = (Brush)properties[random].GetValue(null, null);
+
+            return result;
+        }
+
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            this.Background = PickBrush();
         }
     }
 }
